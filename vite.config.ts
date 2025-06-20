412db2703f0c3a784e36cfbd0f9c4006dfb33651
Hara Keisuke <harakeisuke7@gmail.com>
2025-06-20 23:25:36 +0900

    PWA化対応

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,50 @@
+// vite.config.ts
+
+import { fileURLToPath, URL } from 'node:url' //★
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa' //★
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/typingfall/',
-  plugins: [vue()],
+  // GitHub Pagesなどでサブディレクトリに公開する場合、リポジトリ名を設定
+  // 例: https://<USERNAME>.github.io/sudoku/ -> /sudoku/
+  base: '/typingfall/', 
+  
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate', // 更新があった場合に自動でリロードする
+      devOptions: {
+        enabled: true // 開発モードでもPWAの動作確認を可能にする
+      },
+      manifest: {
+        id: '/typingfall/',
+        name: 'Typing Fall', // アプリのフルネーム
+        short_name: 'TF', // ホーム画面に表示される短い名前
+        description: 'モードが選べるブラインドタッチゲームです。', // アプリの説明
+        start_url: '/typingfall/', // アプリ起動時のURL
+        display: 'standalone', // アドレスバーなどを表示しないネイティブアプリのような表示
+        background_color: '#ffffff', // スプラッシュ画面の背景色
+        theme_color: '#007acc',      // ツールバーの色
+        icons: [
+          {
+            src: 'icon-192x192.png', // publicディレクトリからの相対パス
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icon-512x512.png', // publicディレクトリからの相対パス
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
+  // ビルド時に'@'を'src'として解決するための設定
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 })
